049072f2a4f371cdf7a75a6efa5bf23a4563fe42
Adrien Kühn <adrien.kuhn@gmx.com>
2016-01-19 17:57:23 -0500

    Founders desktop page.

--- src/views/Founders/index.js
+++ src/views/Founders/index.js
@@ -14,12 +14,33 @@ export default class Founders extends Component {
     }
 
     render() {
+        const background = {
+            backgroundImage: 'url(/assets/images/founders.png)',
+            backgroundPosition: '100% 110%',
+            backgroundRepeat: 'no-repeat',
+        };
+
         return (
             <Page>
-                <div className={"container main-content"}>
+                <div className={"container main-content"}
+                    style={background}
+                >
                     <Aside/>
                     <div className={'col-xs-12 col-lg-8'}>
-                        bla
+                        <section className="right-content">
+                            <h2>{'The founders.'}</h2>
+                            <p>
+                                {'I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure'}
+                            </p>
+                            <h3><span className="name">{'Ryall.'}</span> Email me</h3>
+                            <p>
+                                {'I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure'}
+                            </p>
+                            <h3><span className="name">{'Ryall.'}</span> Email me</h3>
+                            <p>
+                                {'I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure'}
+                            </p>
+                        </section>
                     </div>
                 </div>
             </Page>
