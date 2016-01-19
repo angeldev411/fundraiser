216a87b6d4d77fd9ea951416a4f02416dff39d0f
Adrien Kühn <adrien.kuhn@gmx.com>
2016-01-19 14:47:50 -0500

    Remove unused code and comments. Fix footer tagline offset. Add onClick property on Button component.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
 export default class Button extends Component {
+    constructor(props) {
+        super(props);
+        this.propTypes = {
+            onClick: React.PropTypes.func
+        }
+    }
+
     render() {
         return (
             <button
                 type="button"
                 className="btn btn-default"
+                onClick={this.props.onClick}
             >
                 {this.props.children}
             </button>
