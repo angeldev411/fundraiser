7acbe046b8d24043d3c00c2d48154a324dabeb19
Adrien Kühn <adrien.kuhn@gmx.com>
2016-01-21 14:10:41 -0500

    Team Profile right content.

--- src/views/TeamProfile/index.js
+++ src/views/TeamProfile/index.js
@@ -17,15 +17,19 @@ export default class TeamProfile extends Component {
     render() {
         return (
             <Page>
-                <Cover image={"url(/assets/images/team_cover.jpg)"}
+                <Cover image={"url(/assets/images/samples/team_cover.jpg)"}
                     customclass={"cover-profile"}
                     tagline={"I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
                     button={"Sponsor Now"}
                 />
                 <div className={"container main-content"}>
                     <Layout34 page={'teamprofile'}>
-                        <h2>{'YOU + US'}<br/>{'= A WORLD OF CHANGE'}</h2>
-                        <h3>{'How It Works.'}</h3>
+                        <img id="team-logo"
+                            src="/assets/images/samples/team_logo.png"
+                            title=""
+                        />
+                        <h2>{'LOVE + HELP = MONEY FOR THE HOMELESS'}</h2>
+                        <h3>{'Habitat for Humanity'}</h3>
                         <p>
                             {'I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat'}
                         </p>
