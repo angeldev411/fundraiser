6445ada18d435b4c006cd4aa963f22d693368ad7
Adrien Kühn <adrien.kuhn@gmx.com>
2016-01-21 10:42:31 -0500

    Create Layout34 component. How it Works and Founders refacto.

--- src/views/Founders/index.js
+++ src/views/Founders/index.js
@@ -5,7 +5,7 @@ import * as actions from '../../redux/actions';
 
 /* Then React components */
 import Page from '../../components/Page';
-import Aside from '../../components/Aside';
+import Layout34 from '../../components/Layout34';
 
 /* Then view-related stuff */
 export default class Founders extends Component {
@@ -16,24 +16,21 @@ export default class Founders extends Component {
     render() {
         return (
             <Page>
-                <div className={"container main-content founders"}>
-                    <Aside/>
-                    <div className={'col-xs-12 col-lg-8'}>
-                        <section className="right-content">
-                            <h2>{'The founders.'}</h2>
-                            <p>
-                                {'I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure'}
-                            </p>
-                            <h3>{'Ryall.'} <a href="mailto:email@example.com">{'Email me'}</a></h3>
-                            <p>
-                                {'I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure'}
-                            </p>
-                            <h3>{'Joel.'} <a href="mailto:email@example.com">{'Email me'}</a></h3>
-                            <p>
-                                {'I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure'}
-                            </p>
-                        </section>
-                    </div>
+                <div className={"container main-content"}>
+                    <Layout34 page={'founders'}>
+                        <h2>{'The founders.'}</h2>
+                        <p>
+                            {'I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure'}
+                        </p>
+                        <h3>{'Ryall.'} <a href="mailto:email@example.com">{'Email me'}</a></h3>
+                        <p>
+                            {'I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure'}
+                        </p>
+                        <h3>{'Joel.'} <a href="mailto:email@example.com">{'Email me'}</a></h3>
+                        <p>
+                            {'I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure'}
+                        </p>
+                    </Layout34>
                 </div>
             </Page>
         );
