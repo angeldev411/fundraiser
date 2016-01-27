5b280cf0c53e410441575643abdae197a3053346
Adrien Kühn <adrien.kuhn@gmx.com>
2016-01-27 17:34:23 -0500

    Add Google Analytics.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import routes from './routes';
 import { createStore, combineReducers } from 'redux';
 import { syncReduxAndRouter, routeReducer } from 'redux-simple-router';
 import mainReducer from './redux/reducers';
+import ga from 'react-ga';
 
 const reducer = combineReducers({
     main: mainReducer,
@@ -16,14 +17,25 @@ const reducer = combineReducers({
 /* In dev mode, use the logger */
 import { applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
+
 const logger = createLogger();
 const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
 
 export const store = createStoreWithMiddleware(reducer);
+
 /* In production, remove the logger */
 // export const store = createStore(reducer);
-
 const history = createBrowserHistory();
+
+var options = { debug: true };
+ga.initialize('UA-72900949-1', options);
+
+history.listen(location => {
+    if (location.action === "POP" || location.action === "REPLACE") {
+        console.log('Google Analytics', location.pathname);
+        ga.pageview(location.pathname);
+    }
+});
 
 syncReduxAndRouter(history, store);
 
