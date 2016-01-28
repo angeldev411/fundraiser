4a780f55862a5bdbcb5efe1d1c86236e315a420b
Adrien Kühn <adrien.kuhn@gmx.com>
2016-01-28 11:13:32 -0500

    Combine What Is Raiserve and How it Works pages.

--- src/views/RaiserveBasics/index.js
+++ src/views/RaiserveBasics/index.js
@@ -5,18 +5,18 @@ import React, { Component } from 'react';
 import Page from '../../components/Page';
 import Layout34 from '../../components/Layout34';
 
-export default class HowItWorks extends Component {
+export default class RaiserveBasics extends Component {
     componentWillMount() {
-        document.title = 'How It Works | Raiserve';
+        document.title = 'Raiserve Basics | Raiserve';
     }
 
     render() {
         return (
             <Page>
                 <div className={"container main-content no-cover"}>
-                    <Layout34 page={'howitworks'}>
+                    <Layout34 page={'raiserve-basics'}>
                         <h2>{'YOU + US'}<br/>{'= A WORLD OF CHANGE'}</h2>
-                        <h3>{'How It Works.'}</h3>
+                        <h3>{'Raiserve Basics'}</h3>
                         <p>
                             {'I’ve Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat'}
                         </p>
@@ -27,6 +27,6 @@ export default class HowItWorks extends Component {
     }
 }
 
-HowItWorks.propTypes = {
+RaiserveBasics.propTypes = {
     show: React.PropTypes.bool,
 };
