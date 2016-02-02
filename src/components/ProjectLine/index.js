5cc65cbf0851a135034b09a0058b46794e0d8ab5
Adrien Kühn <adrien.kuhn@gmx.com>
2016-02-02 17:46:41 -0500

    Add collapse on Project lines.

--- src/components/ProjectLine/index.js
+++ src/components/ProjectLine/index.js
@@ -3,9 +3,21 @@ import * as constants from '../../common/constants';
 import TeamLine from '../TeamLine';
 
 export default class ProjectLine extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collapsed: true,
+        };
+    }
+
+    toggle = () => {
+        this.setState({collapsed: !this.state.collapsed});
+    };
+
     render() {
         return (
-            <li className="project">
+            <li className="project clearfix">
                 <div className="project-details">
                     <span className="project-name uppercase">Project Name: </span> {this.props.project.name}
                     <a href="#"
@@ -14,17 +26,24 @@ export default class ProjectLine extends Component {
                     <a href="#"
                         className={'uppercase'}
                     >{'New Team'}</a>
-                    <a href="#"
-                        className={'expand pull-right'}
-                    >{'+'}</a>
+                    <button
+                        className={'expand btn-link pull-right'}
+                        onClick={this.toggle}
+                    >{this.state.collapsed ? '+' : '-'}</button>
                 </div>
-                <ul className="teams clearfix">
-                    {this.props.project.teams.map((team, i) => (
-                        <TeamLine key={i}
-                            team={team}
-                        />)
-                    )}
-                </ul>
+                {
+                    this.state.collapsed ?
+                    (null) :
+                    (
+                        <ul className="teams clearfix">
+                            {this.props.project.teams.map((team, i) => (
+                                <TeamLine key={i}
+                                    team={team}
+                                />)
+                            )}
+                        </ul>
+                    )
+                }
             </li>
         );
     }
