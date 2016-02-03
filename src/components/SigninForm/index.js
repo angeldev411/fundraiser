d11e6fe7e9ae2cdf0197655bc67295e51a817f78
Adrien Kühn <adrien.kuhn@gmx.com>
2016-02-03 17:14:56 -0500

    refacto Signin and Signup forms.

--- src/components/SigninForm/index.js
+++ src/components/SigninForm/index.js
@@ -1,32 +1,31 @@
 import React, { Component } from 'react';
 import Button from '../../components/Button';
+import Form from '../../components/Form';
 
 export default class SigninForm extends Component {
     render() {
         return (
-            <div className="form-container col-xs-12 col-md-8 col-md-offset-2">
-                <h2>{'Sign In'}</h2>
-                <p>{'Isicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'}</p>
-                <form>
-                    <div className="form-group">
-                        <input type="email"
-                            name="email"
-                            id="email"
-                        />
-                        <label htmlFor="email">{'Email address'}</label>
-                    </div>
+            <Form title={'Sign In'}
+                description="Isicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+            >
+                <div className="form-group">
+                    <input type="email"
+                        name="email"
+                        id="email"
+                    />
+                    <label htmlFor="email">{'Email address'}</label>
+                </div>
 
-                    <div className="form-group">
-                        <input type="password"
-                            name="password"
-                            id="password"
-                        />
-                        <label htmlFor="password">{'Password'}</label>
-                    </div>
+                <div className="form-group">
+                    <input type="password"
+                        name="password"
+                        id="password"
+                    />
+                    <label htmlFor="password">{'Password'}</label>
+                </div>
 
-                    <Button type="btn-success">{'Sign In'}</Button>
-                </form>
-            </div>
+                <Button type="btn-success">{'Sign In'}</Button>
+            </Form>
         );
     }
 }
--- src/components/SignupForm/index.js
+++ src/components/SignupForm/index.js
@@ -1,42 +1,39 @@
 import React, { Component } from 'react';
 import Button from '../../components/Button';
+import Form from '../../components/Form';
 
 export default class SignupForm extends Component {
     render() {
         return (
-            <div id="signup"
-                className="form-container col-xs-12 col-md-8 col-md-offset-2"
+            <Form id="signup"
+                title={'Sign Up'}
+                description="Isicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
             >
-                <h2 className={'uppercase'}>{'Sign Up'}</h2>
-                <p>{'isicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'}</p>
-                <form>
-                    <div className="form-group">
-                        <input type="email"
-                            name="email"
-                            id="email"
-                        />
-                        <label htmlFor="email">{'Email address'}</label>
-                    </div>
+                <div className="form-group">
+                    <input type="email"
+                        name="email"
+                        id="email"
+                    />
+                    <label htmlFor="email">{'Email address'}</label>
+                </div>
 
-                    <div className="form-group">
-                        <input type="password"
-                            name="password"
-                            id="password"
-                        />
-                        <label htmlFor="password">{'Password'}</label>
-                    </div>
+                <div className="form-group">
+                    <input type="password"
+                        name="password"
+                        id="password"
+                    />
+                    <label htmlFor="password">{'Password'}</label>
+                </div>
 
-                    <div className="form-group">
-                        <input type="password"
-                            name="password-confirmation"
-                            id="password-confirmation"
-                        />
-                        <label htmlFor="password-confirmation">{'Password Confirmation'}</label>
-                    </div>
-
-                    <Button type="btn-success">{'Submit'}</Button>
-                </form>
-            </div>
+                <div className="form-group">
+                    <input type="password"
+                        name="password-confirmation"
+                        id="password-confirmation"
+                    />
+                    <label htmlFor="password-confirmation">{'Password Confirmation'}</label>
+                </div>
+                <Button type="btn-success">{'Submit'}</Button>
+            </Form>
         );
     }
 }
