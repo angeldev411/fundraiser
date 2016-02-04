f141c4ab7a121ba57fc2e427a7339eb3718ea23c
Adrien Kühn <adrien.kuhn@gmx.com>
2016-02-04 11:44:19 -0500

    Remove div.

--- src/components/AdminVolunteersTable/index.js
+++ src/components/AdminVolunteersTable/index.js
@@ -4,35 +4,33 @@ import * as constants from '../../common/constants';
 export default class AdminVolunteersTable extends Component {
     render() {
         return (
-            <div className="volunteers-table">
-                <div className="table-responsive">
-                    <table className="volunteers table">
-                        <thead>
-                            <tr>
-                                <th>Member</th>
-                                <th>Email</th>
-                                <th>Hours</th>
-                                <th>Sponsors</th>
-                                <th>$ Raised</th>
-                                <th>$/Hr</th>
-                                {this.props.editable ? <th>Actions</th> : null}
+            <div className="table-responsive">
+                <table className="volunteers table">
+                    <thead>
+                        <tr>
+                            <th>Member</th>
+                            <th>Email</th>
+                            <th>Hours</th>
+                            <th>Sponsors</th>
+                            <th>$ Raised</th>
+                            <th>$/Hr</th>
+                            {this.props.editable ? <th>Actions</th> : null}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.volunteers.map((volunteer, i) => (
+                            <tr key={i}>
+                                <td className="volunteer-name"><img src={`${constants.USER_IMAGES_FOLDER}/${volunteer.uniqid}/${volunteer.image}`}/>{volunteer.name}</td>
+                                <td>{volunteer.email}</td>
+                                <td>{volunteer.hours}</td>
+                                <td>{volunteer.sponsors}</td>
+                                <td>{volunteer.raised}</td>
+                                <td>{volunteer.hourPledge}</td>
+                                {this.props.editable ? <td>{'Actions'}</td> : null}
                             </tr>
-                        </thead>
-                        <tbody>
-                            {this.props.volunteers.map((volunteer, i) => (
-                                <tr key={i}>
-                                    <td className="volunteer-name"><img src={`${constants.USER_IMAGES_FOLDER}/${volunteer.uniqid}/${volunteer.image}`}/>{volunteer.name}</td>
-                                    <td>{volunteer.email}</td>
-                                    <td>{volunteer.hours}</td>
-                                    <td>{volunteer.sponsors}</td>
-                                    <td>{volunteer.raised}</td>
-                                    <td>{volunteer.hourPledge}</td>
-                                    {this.props.editable ? <td>{'Actions'}</td> : null}
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         );
     }
