7294faef0475c40c8e5b3a925650ef99627ef978
Adrien Thiery <adrien.thiery@gmail.com>
2016-02-05 08:44:32 -0500

    We have a test !

--- server/auth/auth_spec.js
+++ server/auth/auth_spec.js
@@ -1,9 +1,19 @@
 'use strict';
+const app = require('../app');
+const request = require('supertest');
+const fixtures = require('../tests_helpers/fixtures');
+
+const user = fixtures.initialUsers;
 
 describe('Authentication', () => {
-    describe('Login', () => {
+    describe('Login', (done) => {
         it('lets an existing user Log in', () => {
-            
-        })
+            request(app)
+            .post('/api/v1/login', {
+                email: user.email,
+                password: user.password,
+            })
+            .expect(200, done);
+        });
     });
 });
