95f856e1328dddab8abc73c5e710c4389d08131b
Adrien Kühn <adrien.kuhn@gmx.com>
2016-02-11 17:34:22 -0500

    rECORD hours form.

---
+++ src/components/RecordHoursForm/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Button from '../../components/Button';
+import Form from '../../components/Form';
+import DateTimeInput from 'react-bootstrap-datetimepicker';
+
+export default class RecordHoursForm extends Component {
+    render() {
+        return (
+            <Form title={'Record your time'}
+                cols={"col-xs-12 col-md-8 col-md-offset-2"}
+            >
+                <div className="form-group">
+                    <input type="text"
+                        name="place"
+                        id="place"
+                    />
+                    <label htmlFor="place">{'Place volunteered'}</label>
+                </div>
+                <div className="form-group">
+                    <input type="text"
+                        name="hours"
+                        id="hours"
+                    />
+                    <label htmlFor="hours">{'Hours'}</label>
+                </div>
+                <div className="form-group">
+                    <DateTimeInput
+                        inputProps={{
+                            name: 'date',
+                            id: 'date',
+                        }}
+                    />
+                    <label htmlFor="date">{'Date'}</label>
+                </div>
+                <div className="form-group">
+                    <input type="text"
+                        name="supervisor"
+                        id="supervisor"
+                    />
+                    <label htmlFor="supervisor">{'Supervisor'}</label>
+                </div>
+
+                <div className="form-group">
+                    <input type="password"
+                        name="password"
+                        id="password"
+                    />
+                    <label htmlFor="password">{'Password'}</label>
+                </div>
+
+                <Button type="btn-green-white">{'Submit'}</Button>
+            </Form>
+        );
+    }
+}
