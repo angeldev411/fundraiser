cd33b0498841731a0c936ae765767de3d5808691
Adrien Thiery <adrien.thiery@gmail.com>
2016-02-19 14:49:45 -0500

    Refacto route

--- server/user/volunteer/routes.js
+++ server/user/volunteer/routes.js
@@ -1,51 +1,50 @@
 'use strict';
 
-const express = require('express');
+import express from 'express';
+import util from '../../helpers/util';
+import hoursController from '../../hours/controller';
 const router = express.Router();
-const util = require('../../helpers/util');
-const hoursController = require('../../hours/controller');
 
-router.get("/api/v1/volunteers/:uuid.json", function(req, res){
+router.get('/api/v1/volunteers/:uuid.json', (req, res) => {
     volunteer.fetchByUuid(req.params.uuid)
-    .then(function(data){
-        res.send(util.rsSuccess({volunteer: data}));
+    .then((data) => {
+        res.send(util.rsSuccess({ volunteer: data }));
     })
-    .catch(function(err){
-        console.log("error fetching volunteer by uuid " + err);
-        res.send("error fetching voluneer " + err);
-    })
-});
-
-/* just fetch all team members for teams I lead now */
-router.get("/api/v1/leader/volunteers.json", function(req, res){
-    team.fetchVolunteersILead(req.session.user_uuid)
-    .then(function(data){
-        res.send(util.rsSuccess({volunteers: data}));
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send("error " + JSON.stringify(err));
+    .catch((err) => {
+        console.log('error fetching volunteer by uuid ' + err);
+        res.send('error fetching voluneer ' + err);
     });
 });
 
-router.get("/api/v1/volunteering_for_teams.json", function(req, res){
-    user.volunteeringForTeams(req.session.user_uuid)
-    .then(function(result){
-        console.log("successful volunteer teams fetch " + req.session.user_uuid);
-        res.send(util.rsSuccess(result));
-    }.bind(this))
-    .catch(function(err){
-        console.error("post received: errors " + err);
-        res.send(JSON.stringify(util.rsFailure(err)));
+/* just fetch all team members for teams I lead now */
+router.get('/api/v1/leader/volunteers.json', (req, res) => {
+    team.fetchVolunteersILead(req.session.user_uuid)
+    .then((data) => {
+        res.send(util.rsSuccess({ volunteers: data }));
     })
-
+    .catch((err) => {
+        console.log(err);
+        res.send('error ' + JSON.stringify(err));
+    });
 });
 
-router.get("/api/v1/volunteer/my_stats.json", function(req, res){
+router.get('/api/v1/volunteering_for_teams.json', (req, res) => {
+    user.volunteeringForTeams(req.session.user_uuid)
+    .then((result) => {
+        console.log('successful volunteer teams fetch ' + req.session.user_uuid);
+        res.send(util.rsSuccess(result));
+    })
+    .catch((err) => {
+        console.error('post received: errors ' + err);
+        res.send(JSON.stringify(util.rsFailure(err)));
+    });
+});
+
+router.get('/api/v1/volunteer/my_stats.json', (req, res) => {
     res.send('vols path working');
 });
 
-router.post("/api/v1/volunteer/record_hours", (req, res) => {
+router.post('/api/v1/volunteer/record_hours', (req, res) => {
     if (!req.session.user) {
         res.status(403).send('Not logged in');
     }
@@ -56,6 +55,7 @@ router.post("/api/v1/volunteer/record_hours", (req, res) => {
         date: req.body.date,
         supervisorName: req.body.supervisor,
     };
+
     // console.log(hoursController.log(req.session.user.id, hour));
     hoursController.log(req.session.user.id, hour).then((result) => {
         res.status(200).send(result);
@@ -64,13 +64,13 @@ router.post("/api/v1/volunteer/record_hours", (req, res) => {
     });
 });
 
-router.get("/api/v1/volunteer/sponsors.json", function(req, res){
+router.get('/api/v1/volunteer/sponsors.json', (req, res) => {
     volunteer.fetchSponsors(req.session.user_uuid)
-    .then(function(result){
+    .then((result) => {
         res.send(util.rsSuccess(result));
     })
-    .catch(function(err){
-        res.send(util.rsFailure({errors: err, stack: err.stack}));
+    .catch((err) => {
+        res.send(util.rsFailure({ errors: err, stack: err.stack }));
     });
 });
 
