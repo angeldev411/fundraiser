eeed0ae390455839ad2ff0cb8541ea15d061cb66
Adrien Kühn <adrien.kuhn@gmx.com>
2016-02-22 16:51:06 -0500

    Show 0 if team stats not defined.

--- src/components/AdminProjectsTable/index.js
+++ src/components/AdminProjectsTable/index.js
@@ -42,10 +42,10 @@ class AdminProjectsTable extends Component {
                                 <ul className="children-content clearfix">
                                     {project.teams.map((team, x) => (
                                         <ChildrenLine key={x}>
-                                            <span className="label uppercase">Team Name: </span> {team.name}
-                                            <span className="label uppercase">Raised: </span> {team.raised}
-                                            <span className="label uppercase">Average Pledge: </span> {team.pledge}
-                                            <span className="label uppercase">$ / HR: </span> {team.pledgePerHour} / hr
+                                            <span className="label uppercase">{'Team Name: '}</span> {team.name}
+                                            <span className="label uppercase">{'Raised: '}</span> {team.raised ? team.raised : 0}
+                                            <span className="label uppercase">{'Average Pledge: '}</span> {team.pledge ? team.pledge : 0}
+                                            <span className="label uppercase">{'$ / HR: '}</span> {team.pledgePerHour ? team.pledgePerHour : 0} {'/ hr'}
                                             <div className="edit-links">
                                                 <ModalButton customClass="btn-link uppercase"
                                                     content={
@@ -70,7 +70,7 @@ class AdminProjectsTable extends Component {
                             }
                         >
                             <div className="project-details">
-                                <span className="label uppercase">Project Name: </span> {project.name}
+                                <span className="label uppercase">{'Project Name: '}</span> {project.name}
                                 <ModalButton customClass="btn-link uppercase"
                                     content={
                                         <AdminProjectForm title={"Edit Project"}
