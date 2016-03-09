a7522e9c050e3df854ee5caffde82dc960b8acd5
philtrep <phil.trepanier@gmail.com>
2016-03-09 11:27:05 -0500

    Refactored save new team

--- server/team/controller.js
+++ server/team/controller.js
@@ -3,14 +3,27 @@ import Team from './model';
 import messages from '../messages';
 
 class teamController {
-    static store(data, projectSlug, id) {
-        return new Team(data, projectSlug, id)
-        .then((team) => {
-            return Promise.resolve(team);
-        })
-        .catch((err) => {
-            return Promise.reject(err);
-        });
+    static store(data, projectSlug) {
+        let teamObject = null;
+
+        return Team.insert(data.team)
+            .then((team) => {
+                teamObject = team;
+                return Team.linkTeamCreatorAndProject(team.id, data.currentUser.id, projectSlug);
+            })
+            .then(() => {
+                return Team.inviteTeamLeader(teamObject);
+            })
+            .then((result) => {
+                return Promise.resolve(result);
+            })
+            .catch((error) => {
+                return Promise.reject(error);
+            });
+    }
+
+    static update(data) {
+        //
     }
 }
 
