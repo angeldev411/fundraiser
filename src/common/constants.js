4c9197bb2c2f9274e6187de9c61b8fc0c5af9b5d
Adrien Kühn <adrien.kuhn@gmx.com>
2016-03-23 14:40:11 -0400

    fIX DEFAULT LOGO.

--- src/common/constants.js
+++ src/common/constants.js
@@ -5,9 +5,11 @@ export const TWITTER_USERNAME = 'raiserve';
 export const CONTACT_EMAIL = 'contact@raiserve.org';
 export const VOLUNTEER_CONTACT_EMAIL = 'volunteer@raiserve.org';
 
+export const TEAM_IMAGES_FOLDER = '/assets/images/team';
 export const USER_IMAGES_FOLDER = '/assets/images/users';
 export const DEFAULT_AVATAR = 'default-user.png';
 export const DEFAULT_LOGO = 'default-logo.png';
+export const RAISERVE_LOGO = '/assets/images/raiserve_logo.png';
 export const DEFAULT_COVER = 'default-cover.png';
 
 export const USER_PROGRESS_WIDTH = 7.5;
