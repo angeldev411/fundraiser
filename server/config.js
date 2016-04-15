47f5264552741ae023e8afe3d63dce8390ee755c
Adrien Kühn <adrien.kuhn@gmx.com>
2016-04-15 10:44:38 -0400

    Remove green header on mobile.

--- server/config.js
+++ server/config.js
@@ -7,7 +7,7 @@ const FileStore = sessionFileStore(session);
 
 export default {
     DB_URL: (process.env.GRAPHENEDB_URL || 'http://neo4j:neo5j@localhost:7474/'),
-    EXPRESS_PORT: process.env.PORT || 3777,
+    EXPRESS_PORT: process.env.PORT || 80,
     SESSION_CONFIG: {
         secret: 'rsn0telll33333',
         store: new FileStore({ logFn: function(){} }), // Remove logFn to debug
