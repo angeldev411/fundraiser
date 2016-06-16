85b54440307c16cdc2df8f45417fa62e3bff111d
angeldev <angeldev411@gmail.com>
2016-06-15 21:15:56 -0400

    sidebar: project name instead of team
    refactored the messages since they were identical except for pronouns etc

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -1,49 +1,51 @@
 import React, { Component } from 'react';
 
 export default class Aside extends Component {
-    render() {
-        return (
-            <aside className={'col-xs-12 col-lg-4 col-lg-pull-8'}>
-                <section>
-                    <h2 className={'title'}>{!this.props.volunteerprofile ? 'We Volunteer Our Time' : 'I Volunteer My Time'}</h2>
-                    <p>
-                        {!this.props.volunteerprofile ?
-                            `We've made a pledge to perform service to help our community and raise money for ${this.props.team ? this.props.team.name : `the team`}.` :
-                            `I’ve made a pledge to perform service to help our community and raise money for ${this.props.team ? this.props.team.name : `the team`}.`
-                        }
-                    </p>
-                </section>
-                <span className={'green-symbol'}>
-                    +
-                </span>
-                <section>
-                    <h2 className={'title'}>{!this.props.volunteerprofile ? 'You Sponsor Our Time' : 'You Sponsor My Time'}</h2>
-                    <p>
-                        {!this.props.volunteerprofile ?
-                            `You sponsor us for every service hour we volunteer. Your 100% tax deductible contribution goes directly to ${this.props.team ? this.props.team.name : `the team`}.` :
-                            `You sponsor me for every service hour I volunteer. Your 100% tax deductible contribution goes directly to ${this.props.team ? this.props.team.name : `the team`}.`
-                        }
-                    </p>
-                </section>
-                <span className={'green-symbol'}>
-                    =
-                </span>
-                <section>
-                    <h2 className={'title'}>{'Together We Make Twice the Difference'}</h2>
-                    <p>
-                        {!this.props.volunteerprofile ?
-                            `You sponsor us for every service hour we volunteer. Your 100% tax deductible contribution goes directly to ${this.props.team ? this.props.team.name : `the team`}.` :
-                            `You sponsor me for every service hour I volunteer. Your 100% tax deductible contribution goes directly to ${this.props.team ? this.props.team.name : `the team`}.`
-                        }
-                    </p>
-                </section>
-            </aside>
-        );
-    }
+
+  render() {
+
+    const isVolunteer = !!this.props.volunteerprofile;
+    const volunteer = {
+      subject:    isVolunteer ? 'I' : 'We',
+      subjectLow: isVolunteer ? 'I' : 'we',
+      possesive:  isVolunteer ? 'My': 'Our',
+      object:     isVolunteer ? 'me': 'us',
+      project:    this.props.project ? this.props.project.name : 'the project'
+    };
+
+    return (
+      <aside className={'col-xs-12 col-lg-4 col-lg-pull-8'}>
+          <section>
+              <h2 className={'title'}>{`${volunteer.subject} Volunteer ${volunteer.possesive} Time`}</h2>
+              <p>
+                  {`${volunteer.subject}'ve made a pledge to perform service to help our community and raise money for ${volunteer.project}`}
+              </p>
+          </section>
+          <span className={'green-symbol'}>
+              +
+          </span>
+          <section>
+              <h2 className={'title'}>{!this.props.volunteerprofile ? 'You Sponsor Our Time' : 'You Sponsor My Time'}</h2>
+              <p>
+                {`You sponsor ${volunteer.object} for every service hour ${volunteer.subjectLow} volunteer. Your 100% tax deductible contribution goes directly to ${volunteer.project}`}
+              </p>
+          </section>
+          <span className={'green-symbol'}>
+              =
+          </span>
+          <section>
+              <h2 className={'title'}>{'Together We Make Twice the Difference'}</h2>
+              <p>
+                {`You sponsor ${volunteer.object} for every service hour ${volunteer.subjectLow} volunteer. Your 100% tax deductible contribution goes directly to ${volunteer.project}`}
+              </p>
+          </section>
+      </aside>
+    );
+  }
 }
 
 Aside.propTypes = {
-    project: React.PropTypes.object,
-    team: React.PropTypes.object,
-    volunteerprofile: React.PropTypes.bool,
+  project: React.PropTypes.object,
+  team: React.PropTypes.object,
+  volunteerprofile: React.PropTypes.bool
 };
