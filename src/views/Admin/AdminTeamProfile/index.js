cb21e6b334a0e816f94d506070f1c04fec61f8b2
Aaron Lampros <alampros@gmail.com>
2016-07-19 10:37:05 -0400

    Replace Datepicker with react bootstrap datetime
    Fix jsx format.

--- src/views/Admin/AdminTeamProfile/index.js
+++ src/views/Admin/AdminTeamProfile/index.js
@@ -2,8 +2,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
-import DatePicker from 'react-datepicker';
-import './_datepicker.scss';
+import DateTimeInput from 'react-bootstrap-datetimepicker';
+import 'components/_react-bootstrap-datetime-picker.scss';
 
 /* Then React components */
 import Page from '../../../components/Page';
@@ -90,9 +90,9 @@ class AdminTeamProfile extends Component {
     )(this.props.dispatch);
   };
 
-  handleDateChange(momentDate) {
+  handleDateChange(dateString) {
     const nextState = Object.assign({}, this.state)
-    nextState.team.deadline = momentDate.toISOString()
+    nextState.team.deadline = moment(dateString).toISOString()
     this.setState(nextState, () => {
       Actions.updateTeam(
         nextState.team.id,
@@ -149,116 +149,120 @@ class AdminTeamProfile extends Component {
         ];
 
         return (
-            <Page>
-                <AdminLayout pageNav={pageNav}>
-                    <AdminContentHeader
-                        title={'Edit Team Profile'}
-                        description={'Keep an eye on everyone on your team and watch their individual progress grow.'}
-                    />
-                    <div className="edit-team-profile">
-                    <section className={'form-container'}>
-                        <form className={'col-xs-12 col-md-6'}>
-                            <Button
-                                customClass="btn-lg btn-transparent-green"
-                                to={`${Urls.getTeamProfileUrl(this.props.user.project.slug, this.props.user.team.slug)}?edit`}
-                            >
-                                {'Edit the team page'}
-                            </Button>
-                            <Button
-                                onClick={this.requestPassword}
-                                customClass="btn-lg btn-transparent-green"
-                                disabled={this.state.passwordRequested}
-                                noSpinner
-                            >
-                                    {'Change Password'}
-                            </Button>
-                            {this.state.passwordRequested ?
-                                <p className={'action-description'}>
-                                    {'You should receive a reset password email shortly.'}
-                                </p> :
-                                <p className={'action-description'}>{'Optional'}</p>
-                            }
-                            <div className="checkbox">
-                                <input
-                                    type="checkbox"
-                                    name="supervisor-signature"
-                                    id="supervisor-signature"
-                                    value=""
-                                    checked={this.props.user.team.signatureRequired}
-                                    onChange={(e) => {this.changeSupervisorSignatureRequired(e)}}
-                                />
-                                <label
-                                    className="select-label"
-                                    htmlFor="supervisor-signature"
-                                >
-                                    {'Require Supervisor signature'}
-                                </label>
-                                <p className={'action-description action-margin'}>{'for the hours your volunteers execute'}</p>
-                            </div>
-
-                            <div className="checkbox">
-                                <input
-                                    type="checkbox"
-                                    name="leader-signature"
-                                    id="leader-signature"
-                                    value=""
-                                    checked={this.props.user.team.hoursApprovalRequired}
-                                    onChange={(e) => {this.changeHoursApprovalRequired(e)}}
-                                />
-                                <label
-                                    className="select-label"
-                                    htmlFor="leader-signature"
-                                >
-                                    {'Require Team Leader approval'}
-                                </label>
-                                <p className={'action-description action-margin'}>{'for the hours your volunteers execute'}</p>
-                            </div>
-                            <div className="form-group">
-                                <div className="input-group">
-                                    <input
-                                        disabled={this.disabledGoal()}
-                                        type="text"
-                                        name="goal"
-                                        id="goal"
-                                        value={this.props.user.team.goal}
-                                        onChange={(e) => {this.changeGoal(e)}}
-                                    />
-                                    <span className="lock input-group-addon">
-                                                {
-                                                    this.disabledGoal() ?
-                                                    <i className="fa fa-lock" aria-hidden="true"></i>:
-                                                    <i className="fa fa-unlock" aria-hidden="true"></i>
-                                                }
-                                            </span>
-                                </div>
-                                {
-                                    this.disabledGoal() ?
-                                        <label className={'action-description action-margin goal-description'}>{'Note:  Your team already as a sponsor, Goal hours are locked.'}</label>:
-                                        <label className={'action-description action-margin goal-description'}>{'How many total hours is your team aiming for?'} <br/>{'Note: you cannot change your goal hours after you get your first sponsor'}</label>
-                                }
-
-                            </div>
-                            <div className="form-group">
-                            <DatePicker
-                              type="date"
-                              selected={ moment(this.state.team.deadline) }
-                              minDate={moment()}
-                              maxDate={moment().add(1, 'year')}
-                              onChange={ this.handleDateChange }
-                              />
-                            <label className={'action-description action-margin goal-description'}>{'What is the deadline for your team? It can be up to a year from the start date.'}</label>
-                            </div>
-                            <Button
-                                customClass="btn-lg btn-green-white"
-                                to={`${Urls.getTeamProfileUrl(this.props.user.project.slug, this.props.user.team.slug)}`}
-                            >
-                                {'View Team Page'}
-                            </Button>
-                        </form>
-                        </section>
+          <Page>
+            <AdminLayout pageNav={pageNav}>
+              <AdminContentHeader
+                title={'Edit Team Profile'}
+                description={'Keep an eye on everyone on your team and watch their individual progress grow.'}
+              />
+              <div className="edit-team-profile">
+                <section className={'form-container'}>
+                  <form className={'col-xs-12 col-md-6'}>
+                    <Button
+                      customClass="btn-lg btn-transparent-green"
+                      to={`${Urls.getTeamProfileUrl(this.props.user.project.slug, this.props.user.team.slug)}?edit`}
+                    >
+                      {'Edit the team page'}
+                    </Button>
+                    <Button
+                      onClick={this.requestPassword}
+                      customClass="btn-lg btn-transparent-green"
+                      disabled={this.state.passwordRequested}
+                      noSpinner
+                    >
+                      {'Change Password'}
+                    </Button>
+                    {this.state.passwordRequested ? (
+                      <p className={'action-description'}>You should receive a reset password email shortly.</p>
+                    ) : (
+                      <p className={'action-description'}>Optional</p>
+                    )}
+                    <div className="checkbox">
+                      <input
+                        type="checkbox"
+                        name="supervisor-signature"
+                        id="supervisor-signature"
+                        value=""
+                        checked={this.props.user.team.signatureRequired}
+                        onChange={(e) => {this.changeSupervisorSignatureRequired(e)}}
+                      />
+                      <label
+                        className="select-label"
+                        htmlFor="supervisor-signature"
+                      >
+                        Require Supervisor signature
+                      </label>
+                      <p className={'action-description action-margin'}>for the hours your volunteers execute</p>
                     </div>
-                </AdminLayout>
-            </Page>
+
+                    <div className="checkbox">
+                      <input
+                        type="checkbox"
+                        name="leader-signature"
+                        id="leader-signature"
+                        value=""
+                        checked={this.props.user.team.hoursApprovalRequired}
+                        onChange={(e) => {this.changeHoursApprovalRequired(e)}}
+                      />
+                      <label
+                        className="select-label"
+                        htmlFor="leader-signature"
+                      >
+                        Require Team Leader approval
+                      </label>
+                      <p className={'action-description action-margin'}>for the hours your volunteers execute</p>
+                    </div>
+                    <div className="form-group">
+                      <div className="input-group">
+                        <input
+                          disabled={this.disabledGoal()}
+                          type="text"
+                          name="goal"
+                          id="goal"
+                          value={this.props.user.team.goal}
+                          onChange={(e) => {this.changeGoal(e)}}
+                        />
+                        <span className="lock input-group-addon">
+                          {this.disabledGoal() ? (
+                            <i className="fa fa-lock" aria-hidden="true"></i>
+                          ) : (
+                            <i className="fa fa-unlock" aria-hidden="true"></i>
+                          )}
+                        </span>
+                      </div>
+                      {this.disabledGoal() ? (
+                        <label className={'action-description action-margin goal-description'}>Note:  Your team already as a sponsor, Goal hours are locked.</label>
+                      ) : (
+                        <label className={'action-description action-margin goal-description'}>How many total hours is your team aiming for?<br/>Note: you cannot change your goal hours after you get your first sponsor</label>
+                      )}
+                    </div>
+                    <div className="form-group" style={{ position: 'relative' }}>
+                      <DateTimeInput
+                        inputProps={{
+                          name: 'date',
+                          id: 'date',
+                        }}
+                        onChange={this.handleDateChange}
+                        inputFormat={'YYYY-MM-DD'}
+                        format={'YYYY-MM-DD'}
+                        mode={'date'}
+                        minDate={moment()}
+                        maxDate={moment().add(1, 'year')}
+                        dateTime={moment(this.state.team.deadline).format('YYYY-MM-DD')}
+                      />
+                      <label className={'action-description action-margin goal-description'}>What is the deadline for your team? It can be up to a year from the start date.</label>
+                    </div>
+                    <Button
+                      customClass="btn-lg btn-green-white"
+                      to={`${Urls.getTeamProfileUrl(this.props.user.project.slug, this.props.user.team.slug)}`}
+                    >
+                      View Team Page
+                    </Button>
+                  </form>
+                </section>
+              </div>
+            </AdminLayout>
+          </Page>
         );
     }
 }
